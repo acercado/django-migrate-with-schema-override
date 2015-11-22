43a8a206a7c2a732039e61880d4c95daedbbcea6
Ariel Cercado <acercado@risingtide.ph>
2015-11-23 06:25:48 +0800

    some fixes

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
-import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+packages = [
+    'cursor_override',
+]
 
 setup(
     name='django-migrate-with-schema-override',
-    version=0.1,
-    packages=[django_migrate_with_schema_override],
+    version=0.3,
+    packages=packages,
     include_package_data=True,
     license='MIT',
     description='Override migrate command not to trip on tables with schema name provided via db_tables.',
-    long_description=README,
+    long_description=open('README.rst').read(),
     author='Ariel Cercado',
     author_email='ariel.cercado@gmail.com',
-    url='http://github.com/acercado/django_migrate_with_schema_override',
+    url='https://github.com/acercado/django-migrate-with-schema-override',
     classifiers=[
-        'Environment :: Web Development',
+        'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
